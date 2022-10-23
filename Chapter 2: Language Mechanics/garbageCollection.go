b7447ddd131a071cd1f6a9b7004036d88e12973d
Ross Currie <ross.currie@teamwork.com>
2022-10-23 17:38:06 +0100

    garbageCollector notes

---
+++ Chapter 2: Language Mechanics/garbageCollection.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Garbage Collection Points")
+
+	// Garbage Collector (GC) gets involved when value is constructed on heap. Pacing algorithm is the most important part
+	// of GC <- determines the pace that GC must run to maintain heap, should be the smallest heap possible in conjunction
+	// with the best app throughput.
+}
